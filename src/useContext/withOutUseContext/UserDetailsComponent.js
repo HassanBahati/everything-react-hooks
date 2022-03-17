b2df69ac6e06e272277a42653f1fd9230be3afba
bahati.mukisa <bahati.mukisa@outbox.co.ug>
2022-03-17 12:33:16 +0300

    create context object and provider

--- src/useContext/withOutUseContext/UserDetailsComponent.js
+++ src/useContext/withOutUseContext/UserDetailsComponent.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ChildComponent from "./ChildComponent";
 
 function UserDetailsComponent() {
-  var [userDetails, setUserDetails] = useState({
+  const [userDetails, setUserDetails] = useState({
     name: "John Doe",
     age: 23,
   });
